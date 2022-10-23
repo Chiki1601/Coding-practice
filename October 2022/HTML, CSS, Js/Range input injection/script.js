95bebe55169b564eb16421671140357465fb7802
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-23 21:02:52 +0530

    Add files via upload

---
+++ October 2022/HTML, CSS, Js/Range input injection/script.js
@@ -0,0 +1,11 @@
+// inspired by sakin kumar's dribbble shot
+// https://dribbble.com/shots/4396605-Dribbble-Sticker-Addiction
+
+// todo:
+// enable dragging by the handle
+
+function range(value) {
+	var val = value + '%';
+	console.log(val);
+	$('.fluid').css('width', val);
+}
