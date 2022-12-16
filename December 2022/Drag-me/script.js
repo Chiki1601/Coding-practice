d496ba12b5682dd4da1a87bb878bcd0d0404737a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-12-17 01:48:50 +0530

    Create script.js

---
+++ December 2022/Drag-me/script.js
@@ -0,0 +1,75 @@
+let draggableElem = document.getElementById("draggable-elem");
+let initialX = 0,
+  initialY = 0;
+let moveElement = false;
+
+//Events Object
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+let deviceType = "";
+
+//Detech touch device
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent (it would fail for desktops and throw error)
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+isTouchDevice();
+
+//Start (mouse down / touch start)
+draggableElem.addEventListener(events[deviceType].down, (e) => {
+  e.preventDefault();
+  //initial x and y points
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  //Start movement
+  moveElement = true;
+});
+
+//Move
+draggableElem.addEventListener(events[deviceType].move, (e) => {
+  //if movement == true then set top and left to new X andY while removing any offset
+  if (moveElement) {
+    e.preventDefault();
+    let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+    let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+    draggableElem.style.top =
+      draggableElem.offsetTop - (initialY - newY) + "px";
+    draggableElem.style.left =
+      draggableElem.offsetLeft - (initialX - newX) + "px";
+    initialX = newX;
+    initialY = newY;
+  }
+});
+
+//mouse up / touch end
+draggableElem.addEventListener(
+  events[deviceType].up,
+  (stopMovement = (e) => {
+    moveElement = false;
+  })
+);
+
+draggableElem.addEventListener("mouseleave", stopMovement);
+draggableElem.addEventListener(events[deviceType].up, (e) => {
+  moveElement = false;
+});
